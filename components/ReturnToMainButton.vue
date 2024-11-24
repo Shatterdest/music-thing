3b0140589d78a012d70a6d90ab08d8482dc690be
Batyrkhan Alimzhanov <144455676+batyrkhana@users.noreply.github.com>
2024-11-24 14:05:09 -0500

    Website UI
    What's New:
    - Buttons converted into components
    - Login Page Added
    - SignUp Page Added
    - Index Page Finished
    - Fixed Scroll Issues
    - Added a repeating background

---
+++ components/ReturnToMainButton.vue
@@ -0,0 +1,30 @@
+<template>
+    <button class="returntomainBtn">
+      Return to Main Menu
+    </button>
+  </template>
+  
+  <script setup>
+  </script>
+  
+  <style scoped>
+  .returntomainBtn {
+      background-color: #B8D0F8;
+      width: 860px;
+      height: 95px;
+      border-radius: 30px;
+      border-color: black;
+      border-style: solid;
+      border-width: 10px;
+      margin-left: 40px;
+      margin-top: 80px;
+      font-size: 80px;
+      font-family: 'Pixelify Sans', sans-serif;
+      display: flex;
+      justify-content: center; 
+      align-items: center; 
+      text-align: center; 
+      line-height: 1;
+      cursor: pointer;
+  }
+  </style>
