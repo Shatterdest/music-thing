ec63f0493d8e22ee39b69ce336b709054bc6d669
Shatterdest <73318855+Shatterdest@users.noreply.github.com>
2024-11-25 13:41:19 -0500

    auth integrated into store

--- stores/user.ts
+++ stores/user.ts
@@ -1,3 +1,7 @@
+import { defineStore } from 'pinia'
+import { ref } from 'vue'
+
+
 export const useUserStore = defineStore("userStore", () => {
 
     const email = ref('')
@@ -6,4 +10,13 @@ export const useUserStore = defineStore("userStore", () => {
     const refreshToken = ref('')  
     const pokemon = ref<(PokemonObject)[]>([]);
 
+    function $reset() {
+        email.value = ''
+        displayName.value = ''
+        token.value = ''
+        refreshToken.value = ''
+        pokemon.value = []
+    }
+
+    return { email, displayName, token, refreshToken, pokemon, $reset }
 });
