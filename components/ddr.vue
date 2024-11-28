4afe03a1cc6c19aaea16584046596a1cb0bd012f
GhTmp1A <stanbutter4@gmail.com>
2024-11-28 18:57:20 -0500

    Progress on timing.
    Hitting a key now lines up with an arrow hitting the bottom arrow. Now, multiple arrows will have to fall at different times.

--- components/ddr.vue
+++ components/ddr.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="h-screen border border-red-600">
-    Arrow: {{ stillArrow }}
     <div class="relative h-72">
       <div class="h-full w-2/5 border border-black">
         <div id="dynamicArrow" class="absolute animate-fall">
@@ -13,50 +12,37 @@
 </template>
 
 <script setup lang="js">
-onMounted(() => {
-  stillArrow.value = document.getElementById("staticArrow").getBoundingClientRect().top.toFixed(0);
-});
+onMounted(() => {});
 
-const fallArrow = ref();
-const stillArrow = ref();
+onNuxtReady(async () => {
+  const moveArrow = ref(document.getElementById("dynamicArrow").getBoundingClientRect().top.toFixed(0));
+  const targetArrow = ref(document.getElementById("staticArrow").getBoundingClientRect().top.toFixed(0));
 
-async function testFall() {
-  fallArrow.value = document.getElementById("dynamicArrow").getBoundingClientRect().top.toFixed(0);
-  if (fallArrow.value >= 150 && fallArrow.value < 151) {
-    console.log(fallArrow.value);
-  }
-}
-
-setInterval(checkPosition, 20);
-
-testFall();
-
-/* 
-
-function trackElementPosition() {
-  // Get the element you want to track
-  const element = document.getElementById('movingElement');
-
-  // Function to check the element's position
-  function checkPosition() {
-    // Get the current left position of the element (in pixels)
-    const elementPosition = element.getBoundingClientRect().left;
-
-    // Check if the position is 50px
-    if (elementPosition >= 50 && elementPosition < 51) {
-      console.log("yes");
+  //The bottom function may not be necessary
+  function testFall() {
+    const still = ref(document.getElementById("staticArrow").getBoundingClientRect().top.toFixed(0));
+    const fall = ref(document.getElementById("dynamicArrow").getBoundingClientRect().top.toFixed(0));
+    if (fall.value == still.value) {
+      //console.log(`Hit. ${fall.value}`);
     }
+    requestAnimationFrame(testFall);
   }
 
-  // Check position periodically (every 20 milliseconds)
-  setInterval(checkPosition, 20);
-}
+  requestAnimationFrame(testFall);
 
+  window.addEventListener("keydown", (e) => {
+    if (e.key == "ArrowDown" && !e.repeat) {
+      const moveArrow = ref(document.getElementById("dynamicArrow").getBoundingClientRect().top.toFixed(0));
+      const targetArrow = ref(document.getElementById("staticArrow").getBoundingClientRect().top.toFixed(0));
 
-
-
-
- */
+      if (moveArrow.value >= targetArrow.value - 10) {
+        console.log("Hit.");
+      } else {
+        console.log("Miss");
+      }
+    }
+  });
+});
 </script>
 
 <style scoped></style>
