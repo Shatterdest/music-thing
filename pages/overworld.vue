acce84ba3d4aa05ca3195361163a37973ad31ba5
yi <73318855+Shatterdest@users.noreply.github.com>
2024-12-04 10:33:30 -0500

    perchance

--- pages/overworld.vue
+++ pages/overworld.vue
@@ -90,269 +90,269 @@ onMounted(() => {
         showDDR.value = false;
         console.log(userStore.latestPokemon);
         if (userStore.rhythmScore === 0) {
-  alert(userStore.latestPokemon.name + " fled :(");
-} else if (userStore.rhythmScore <= 5) {
-  const chanceOfCatch = Math.floor(Math.random() * 4);
-  if (chanceOfCatch < 1) {
-    alert(userStore.latestPokemon.name + " was caught! :)");
-    userStore.addPokemon(userStore.latestPokemon.id - 1);
-  } else {
-    alert(userStore.latestPokemon.name + " fled :(");
-  }
-} else if (userStore.rhythmScore <= 10) {
-  const chanceOfCatch = Math.floor(Math.random() * 4);
-  if (chanceOfCatch < 2) {
-    alert(userStore.latestPokemon.name + " was caught! :)");
-    userStore.addPokemon(userStore.latestPokemon.id - 1);
-  } else {
-    alert(userStore.latestPokemon.name + " fled :(");
-  }
-} else if (userStore.rhythmScore <= 18) {
-  const chanceOfCatch = Math.floor(Math.random() * 4);
-  if (chanceOfCatch < 3) {
-    alert(userStore.latestPokemon.name + " was caught! :)");
-    userStore.addPokemon(userStore.latestPokemon.id - 1);
-  } else {
-    alert(userStore.latestPokemon.name + " fled :(");
-  }
-} else {
-  alert(userStore.latestPokemon.name + " was caught! :)");
-  userStore.addPokemon(userStore.latestPokemon.id - 1);
-}
-
+          alert(userStore.latestPokemon.name + " fled :(");
+        } else if (userStore.rhythmScore <= 5) {
+          const chanceOfCatch = Math.floor(Math.random() * 4);
+          if (chanceOfCatch < 1) {
+            alert(userStore.latestPokemon.name + " was caught! :)");
+            userStore.addPokemon(userStore.latestPokemon.id - 1);
+          } else {
+            alert(userStore.latestPokemon.name + " fled :(");
+          }
+        } else if (userStore.rhythmScore <= 10) {
+          const chanceOfCatch = Math.floor(Math.random() * 4);
+          if (chanceOfCatch < 2) {
+            alert(userStore.latestPokemon.name + " was caught! :)");
+            userStore.addPokemon(userStore.latestPokemon.id - 1);
+          } else {
+            alert(userStore.latestPokemon.name + " fled :(");
+          }
+        } else if (userStore.rhythmScore <= 18) {
+          const chanceOfCatch = Math.floor(Math.random() * 4);
+          if (chanceOfCatch < 3) {
+            alert(userStore.latestPokemon.name + " was caught! :)");
+            userStore.addPokemon(userStore.latestPokemon.id - 1);
+          } else {
+            alert(userStore.latestPokemon.name + " fled :(");
+          }
+        } else {
+          alert(userStore.latestPokemon.name + " was caught! :)");
+          userStore.addPokemon(userStore.latestPokemon.id - 1);
+        }
       }
     }
   }, 1000);
-
-  if ( window.innerWidth !== undefined && window.innerHeight !== undefined && canvas.value && canvas.value.getContext) {
-    const ctx = canvas.value.getContext("2d");
-    canvas.value.width = window.innerWidth;
-    canvas.value.height = window.innerHeight;
-
-    const resizeHandler = () => {
+  if (import.meta.client) {
+    if (window.innerWidth !== undefined && window.innerHeight !== undefined && canvas.value && canvas.value.getContext) {
+      const ctx = canvas.value.getContext("2d");
       canvas.value.width = window.innerWidth;
       canvas.value.height = window.innerHeight;
-    };
-    window.addEventListener("resize", resizeHandler);
 
-    class Sprite {
-      constructor({ position, image, frames = { max: 1, hold: 12 }, sprites, animate = false, scale = 3 }) {
-        this.position = position;
-        this.image = new Image();
-        this.frames = { ...frames, val: 0, elapsed: 0 };
-        this.image.onload = () => {
-          this.width = (this.image.width / this.frames.max) * this.scale;
-          this.height = this.image.height * this.scale;
-        };
-        this.image.src = image.src;
+      const resizeHandler = () => {
+        canvas.value.width = window.innerWidth;
+        canvas.value.height = window.innerHeight;
+      };
+      window.addEventListener("resize", resizeHandler);
 
-        this.animate = animate;
-        this.sprites = sprites;
-        this.scale = scale;
-      }
+      class Sprite {
+        constructor({ position, image, frames = { max: 1, hold: 12 }, sprites, animate = false, scale = 3 }) {
+          this.position = position;
+          this.image = new Image();
+          this.frames = { ...frames, val: 0, elapsed: 0 };
+          this.image.onload = () => {
+            this.width = (this.image.width / this.frames.max) * this.scale;
+            this.height = this.image.height * this.scale;
+          };
+          this.image.src = image.src;
 
-      draw() {
-        const crop = {
-          x: this.animate ? this.frames.val * (this.image.width / this.frames.max) : 0,
-          y: 0,
-          width: this.image.width / this.frames.max,
-          height: this.image.height
-        };
+          this.animate = animate;
+          this.sprites = sprites;
+          this.scale = scale;
+        }
 
-        ctx.drawImage(this.image, crop.x, crop.y, crop.width, crop.height, this.position.x, this.position.y, crop.width * this.scale, crop.height * this.scale);
+        draw() {
+          const crop = {
+            x: this.animate ? this.frames.val * (this.image.width / this.frames.max) : 0,
+            y: 0,
+            width: this.image.width / this.frames.max,
+            height: this.image.height
+          };
 
-        if (!this.animate) return;
+          ctx.drawImage(this.image, crop.x, crop.y, crop.width, crop.height, this.position.x, this.position.y, crop.width * this.scale, crop.height * this.scale);
 
-        this.frames.elapsed++;
-        if (this.frames.elapsed % this.frames.hold === 0) {
-          this.frames.val = (this.frames.val + 1) % this.frames.max;
+          if (!this.animate) return;
+
+          this.frames.elapsed++;
+          if (this.frames.elapsed % this.frames.hold === 0) {
+            this.frames.val = (this.frames.val + 1) % this.frames.max;
+          }
         }
       }
-    }
 
-    class Boundary {
-      constructor({ position }) {
-        this.position = position;
-        this.width = 16 * mapScale;
-        this.height = 15 * mapScale;
-      }
-      draw() {
-        ctx.fillRect(this.position.x, this.position.y, this.width, this.height, "red");
-      }
-    }
-
-    const REFERENCE_WIDTH = 1494;
-    const REFERENCE_HEIGHT = 742;
-
-    const SCALE_X = window.innerWidth / REFERENCE_WIDTH;
-    const SCALE_Y = window.innerHeight / REFERENCE_HEIGHT;
-
-    const OFFSET_X = 0.731 * REFERENCE_WIDTH * SCALE_X;
-    const OFFSET_Y = 1.9 * REFERENCE_HEIGHT * SCALE_Y;
-
-    const background = new Sprite({
-      position: { x: -OFFSET_X, y: -OFFSET_Y },
-      image: { src: "map.png" }
-    });
-
-    const foreground = new Sprite({
-      position: { x: -OFFSET_X, y: -OFFSET_Y },
-      image: { src: "foreground.png" }
-    });
-
-    const player = new Sprite({
-      position: { x: canvas.value.width / 2 - 16, y: canvas.value.height / 2 - 24 },
-      image: { src: "/player/player_forward.png" },
-      frames: { max: 3, hold: 6 },
-      sprites: {
-        up: { src: "/player/player_back.png" },
-        left: { src: "/player/player_left.png" },
-        down: { src: "/player/player_forward.png" },
-        right: { src: "/player/player_right.png" }
-      },
-      animate: false,
-      scale: 1
-    });
-
-    const boundaries = [];
-    const encounters = [];
-    const tileSize = 16 * mapScale;
-
-    map.forEach((row, i) => {
-      row.forEach((tile, j) => {
-        if (tile === 1) {
-          boundaries.push(
-            new Boundary({
-              position: {
-                x: j * tileSize - (OFFSET_X - mapScale * 60),
-                y: i * tileSize - (OFFSET_Y - mapScale * 80)
-              }
-            })
-          );
+      class Boundary {
+        constructor({ position }) {
+          this.position = position;
+          this.width = 16 * mapScale;
+          this.height = 15 * mapScale;
         }
+        draw() {
+          ctx.fillRect(this.position.x, this.position.y, this.width, this.height, "red");
+        }
+      }
+
+      const REFERENCE_WIDTH = 1494;
+      const REFERENCE_HEIGHT = 742;
+
+      const SCALE_X = window.innerWidth / REFERENCE_WIDTH;
+      const SCALE_Y = window.innerHeight / REFERENCE_HEIGHT;
+
+      const OFFSET_X = 0.731 * REFERENCE_WIDTH * SCALE_X;
+      const OFFSET_Y = 1.9 * REFERENCE_HEIGHT * SCALE_Y;
+
+      const background = new Sprite({
+        position: { x: -OFFSET_X, y: -OFFSET_Y },
+        image: { src: "map.png" }
       });
-    });
 
-    grass.forEach((row, i) => {
-      row.forEach((tile, j) => {
-        if (tile === 1) {
-          encounters.push(
-            new Boundary({
-              position: {
-                x: j * tileSize - (OFFSET_X - mapScale),
-                y: i * tileSize - (OFFSET_Y - mapScale * 80)
-              }
-            })
-          );
-        }
+      const foreground = new Sprite({
+        position: { x: -OFFSET_X, y: -OFFSET_Y },
+        image: { src: "foreground.png" }
       });
-    });
 
-    const keys = { up: false, down: false, left: false, right: false };
-    let lastKeyPressed = "";
+      const player = new Sprite({
+        position: { x: canvas.value.width / 2 - 16, y: canvas.value.height / 2 - 24 },
+        image: { src: "/player/player_forward.png" },
+        frames: { max: 3, hold: 6 },
+        sprites: {
+          up: { src: "/player/player_back.png" },
+          left: { src: "/player/player_left.png" },
+          down: { src: "/player/player_forward.png" },
+          right: { src: "/player/player_right.png" }
+        },
+        animate: false,
+        scale: 1
+      });
 
-    const keyMap = {
-      w: "up",
-      s: "down",
-      a: "left",
-      d: "right"
-    };
+      const boundaries = [];
+      const encounters = [];
+      const tileSize = 16 * mapScale;
 
-    function isCollision(obj1, obj2) {
-      return (
-        obj1.position.x < obj2.position.x + obj2.width &&
-        obj1.position.x + obj1.width > obj2.position.x &&
-        obj1.position.y < obj2.position.y + obj2.height &&
-        obj1.position.y + obj1.height > obj2.position.y
-      );
-    }
-    let moving = false;
-
-    window.addEventListener("keydown", (event) => {
-      const direction = keyMap[event.key];
-      moving = true;
-      if (direction && lastKeyPressed !== direction) {
-        lastKeyPressed = direction;
-
-        player.image.src = player.sprites[direction].src;
-        player.animate = true;
-
-        Object.keys(keys).forEach((key) => {
-          keys[key] = key === direction;
+      map.forEach((row, i) => {
+        row.forEach((tile, j) => {
+          if (tile === 1) {
+            boundaries.push(
+              new Boundary({
+                position: {
+                  x: j * tileSize - (OFFSET_X - mapScale * 60),
+                  y: i * tileSize - (OFFSET_Y - mapScale * 80)
+                }
+              })
+            );
+          }
         });
-      }
-    });
+      });
 
-    window.addEventListener("keyup", () => {
-      lastKeyPressed = "";
-      player.animate = false;
-    });
+      grass.forEach((row, i) => {
+        row.forEach((tile, j) => {
+          if (tile === 1) {
+            encounters.push(
+              new Boundary({
+                position: {
+                  x: j * tileSize - (OFFSET_X - mapScale),
+                  y: i * tileSize - (OFFSET_Y - mapScale * 80)
+                }
+              })
+            );
+          }
+        });
+      });
 
-    function handleMovement(deltaTime, speed) {
-      const movementOffsets = {
-        up: { x: 0, y: -speed * deltaTime },
-        down: { x: 0, y: speed * deltaTime },
-        left: { x: -speed * deltaTime, y: 0 },
-        right: { x: speed * deltaTime, y: 0 }
+      const keys = { up: false, down: false, left: false, right: false };
+      let lastKeyPressed = "";
+
+      const keyMap = {
+        w: "up",
+        s: "down",
+        a: "left",
+        d: "right"
       };
 
-      if (lastKeyPressed && keys[lastKeyPressed]) {
-        const offset = movementOffsets[lastKeyPressed];
+      function isCollision(obj1, obj2) {
+        return (
+          obj1.position.x < obj2.position.x + obj2.width &&
+          obj1.position.x + obj1.width > obj2.position.x &&
+          obj1.position.y < obj2.position.y + obj2.height &&
+          obj1.position.y + obj1.height > obj2.position.y
+        );
+      }
+      let moving = false;
 
-        const isColliding = boundaries.some((boundary) => {
-          const futurePlayer = {
-            position: {
-              x: player.position.x + offset.x,
-              y: player.position.y + offset.y
-            },
-            width: player.width,
-            height: player.height
-          };
-          return isCollision(futurePlayer, boundary);
-        });
+      window.addEventListener("keydown", (event) => {
+        const direction = keyMap[event.key];
+        moving = true;
+        if (direction && lastKeyPressed !== direction) {
+          lastKeyPressed = direction;
 
-        if (!isColliding) {
-          moving = true;
+          player.image.src = player.sprites[direction].src;
+          player.animate = true;
 
-          background.position.x -= offset.x;
-          background.position.y -= offset.y;
-          foreground.position.x -= offset.x;
-          foreground.position.y -= offset.y;
-
-          boundaries.forEach((tile) => {
-            tile.position.x -= offset.x;
-            tile.position.y -= offset.y;
-          });
-
-          encounters.forEach((tile) => {
-            tile.position.x -= offset.x;
-            tile.position.y -= offset.y;
+          Object.keys(keys).forEach((key) => {
+            keys[key] = key === direction;
           });
         }
+      });
 
-        // Check if player is on grass
-        const onGrass = encounters.some((tile) => isCollision(player, tile));
-        if (onGrass && moving) {
-          userStore.onGrass = true;
-        } else {
-          userStore.onGrass = false;
+      window.addEventListener("keyup", () => {
+        lastKeyPressed = "";
+        player.animate = false;
+      });
+
+      function handleMovement(deltaTime, speed) {
+        const movementOffsets = {
+          up: { x: 0, y: -speed * deltaTime },
+          down: { x: 0, y: speed * deltaTime },
+          left: { x: -speed * deltaTime, y: 0 },
+          right: { x: speed * deltaTime, y: 0 }
+        };
+
+        if (lastKeyPressed && keys[lastKeyPressed]) {
+          const offset = movementOffsets[lastKeyPressed];
+
+          const isColliding = boundaries.some((boundary) => {
+            const futurePlayer = {
+              position: {
+                x: player.position.x + offset.x,
+                y: player.position.y + offset.y
+              },
+              width: player.width,
+              height: player.height
+            };
+            return isCollision(futurePlayer, boundary);
+          });
+
+          if (!isColliding) {
+            moving = true;
+
+            background.position.x -= offset.x;
+            background.position.y -= offset.y;
+            foreground.position.x -= offset.x;
+            foreground.position.y -= offset.y;
+
+            boundaries.forEach((tile) => {
+              tile.position.x -= offset.x;
+              tile.position.y -= offset.y;
+            });
+
+            encounters.forEach((tile) => {
+              tile.position.x -= offset.x;
+              tile.position.y -= offset.y;
+            });
+          }
+
+          // Check if player is on grass
+          const onGrass = encounters.some((tile) => isCollision(player, tile));
+          if (onGrass && moving) {
+            userStore.onGrass = true;
+          } else {
+            userStore.onGrass = false;
+          }
         }
       }
+
+      function animate() {
+        const speed = 5;
+        const deltaTime = 1;
+
+        handleMovement(deltaTime, speed);
+        background.draw();
+        foreground.draw();
+        player.draw();
+
+        requestAnimationFrame(animate);
+      }
+
+      animate();
     }
-
-    function animate() {
-      const speed = 5;
-      const deltaTime = 1;
-
-      handleMovement(deltaTime, speed);
-      background.draw();
-      foreground.draw();
-      player.draw();
-
-      requestAnimationFrame(animate);
-    }
-
-    animate();
   }
   function wait(seconds) {
     return new Promise((resolve) => setTimeout(resolve, seconds * 1000));
